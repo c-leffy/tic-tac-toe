72f35c832802d2937fc5b1d6d3cd0318d972bc9e
Corentin Leffy <corentin.leffy@gmail.com>
2026-02-03 21:21:53 +0100

    test(game): Verify that player can navigate to board

--- lib/game/presentation/view/home_page.dart
+++ lib/game/presentation/view/home_page.dart
@@ -1,15 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:tic_tac_toe/core/l10n/core_l10n.dart';
+import 'package:tic_tac_toe/core/presentation/widgets/neo_button.dart';
+import 'package:tic_tac_toe/core/style/size.dart';
+import 'package:tic_tac_toe/game/presentation/l10n/game_l10n.dart';
+import 'package:tic_tac_toe/game/presentation/viewmodel/home_notifier.dart';
+import 'package:tic_tac_toe/game/presentation/widgets/game/difficulty_dialog.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key, required this.title});
-
-  final String title;
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen(homeNotifierProvider, (_, state) {
+      if (state == HomeState.gameStarted) {
+        ref.read(homeNotifierProvider.notifier).reset();
+        context.go('/game');
+      }
+    });
+
+    final isLoading = ref.watch(homeNotifierProvider) == HomeState.loading;
+
     return Scaffold(
-      appBar: AppBar(backgroundColor: Theme.of(context).colorScheme.inversePrimary, title: Text(title)),
-      body: Center(),
+      body: LayoutBuilder(
+        builder: (context, constraints) => SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(minHeight: constraints.maxHeight),
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                spacing: Size.large,
+                children: [
+                  ConstrainedBox(constraints: const BoxConstraints(maxWidth: 400), child: Image.asset('assets/home.png')),
+                  isLoading
+                      ? const CircularProgressIndicator()
+                      : NeoButton.text(label: context.l10n.play, onPressed: () => _onPlayPressed(context, ref)),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
+  }
+
+  Future<void> _onPlayPressed(BuildContext context, WidgetRef ref) async {
+    final difficulty = await DifficultyDialog.show(context);
+    if (difficulty == null) return;
+
+    ref.read(homeNotifierProvider.notifier).startGame(difficulty);
   }
 }
