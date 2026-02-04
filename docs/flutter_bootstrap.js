807a1380172f159fd5e37b19cafaa82d72ed76ee
Corentin Leffy <corentin.leffy@gmail.com>
2026-02-04 08:48:41 +0100

    Add web build for GitHub Pages

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "29889369"
+    serviceWorkerVersion: "836829623"
   }
 });
